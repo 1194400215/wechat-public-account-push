631cd431f6273f8f8718952b47ebaf7c68de90df
小黑 <112531118+1194400215@users.noreply.github.com>
2022-09-25 20:53:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'om3lU6ols0FqxiD5BxuHkW95CpiE',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '1qa15xMKSpYxEJnGAVaNi4DDiKOxzbY0vUMwNLkVUO0	',
+      useTemplateId: [1qa15xMKSpYxEJnGAVaNi4DDiKOxzbY0vUMwNLkVUO0	,
       // 所在省份
       province: '陕西',
       // 所在城市
